ed6be9dce3787ea925a8847cd277eb09c2c651a9
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2016-12-17 20:47:15 +1100

    055 Formset for Inventory

---
+++ src/products/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.forms.models import modelformset_factory
+
+from .models import Variation
+
+class VaraitionInventoryForm(forms.ModelForm):
+    class Meta:
+        model = Variation
+        fields = [
+            "title",
+            "price",
+            "sale_price",
+            "inventory",
+            "active"
+        ]
+
+VaraitionInventoryFormSet = modelformset_factory(Variation, form=VaraitionInventoryForm, extra=1)
--- src/products/views.py
+++ src/products/views.py
@@ -1,16 +1,55 @@
+from django.contrib import messages
 from django.db.models import Q
 from django.http import Http404
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
 # Create your views here.
 
-from .models import Product
+from .forms import VaraitionInventoryFormSet
+from .models import Product, Variation
+
+
+class VariationListView(ListView):
+    model = Variation
+    queryset = Variation.objects.all()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(VariationListView, self).get_context_data(*args, **kwargs)
+        context["formset"] = VaraitionInventoryFormSet(queryset=self.get_queryset())
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        query_set = super(VariationListView, self).get_queryset(*args, **kwargs)
+        query = self.request.GET.get("q")
+        product_pk = self.kwargs.get("pk")
+        if product_pk:
+            product = get_object_or_404(Product, pk=product_pk)
+            query_set = Variation.objects.filter(product=product)
+        return query_set
+
+    def post(self, request, *args, **kwargs):
+        formset = VaraitionInventoryFormSet(request.POST, request.FILES)
+        print request.POST
+        if formset.is_valid():
+            formset.save(commit=False)
+            for form in formset:
+                new_item = form.save(commit=False)
+                product_pk = self.kwargs.get("pk")
+                product = get_object_or_404(Product, pk=product_pk)
+                new_item.product = product
+                new_item.save()
+
+            messages.success(request, "Your inventory and pricing has been updated")
+            return redirect("products")
+        print request.POST
+        raise Http404
 
 class ProductListView(ListView):
     model = Product
+    queryset = Product.objects.all()
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductListView, self).get_context_data(*args, **kwargs)
