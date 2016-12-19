78ca982adb2788bfa4c12c932a324d4731fd7a31
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2016-12-20 01:24:01 +1100

    062 Category Detail View

--- src/products/models.py
+++ src/products/models.py
@@ -92,3 +92,8 @@ class Category(models.Model):
 
     def __unicode__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("category_detail", kwargs={
+            "slug" : self.slug
+        })
