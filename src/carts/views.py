aef40c673ff89bb5259020a09afeff7a995806aa
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2016-12-22 22:40:28 +1100

    084 Ajax Part 2

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,3 +1,4 @@
+from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, Http404, JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.base import View
@@ -56,11 +57,27 @@ class CartView(SingleObjectMixin, View):
                 cart_item.quantity = qty
                 cart_item.save()
 
+            if not request.is_ajax():
+                return HttpResponseRedirect(reverse("cart"))
+
         if request.is_ajax():
-            return JsonResponse({
+            try:
+                total = cart_item.line_item_total
+            except:
+                total = None
+            try:
+                subtotal = cart_item.cart.subtotal
+            except:
+                subtotal = None
+
+            data = JsonResponse({
                 "deleted": delete_item,
-                "item_added": item_added
+                "item_added": item_added,
+                "line_total": total,
+                "subtotal": subtotal
             })
+
+            return data
 
         context = {
             "object": self.get_object()
