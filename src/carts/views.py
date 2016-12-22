3a6dab5ccbe46760d09fd6e690d23230849281f5
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2016-12-23 03:26:56 +1100

    090 Checkout View

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.forms import AuthenticationForm
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, Http404, JsonResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic.base import View
-from django.views.generic.detail import SingleObjectMixin
+from django.views.generic.detail import SingleObjectMixin, DetailView
 
 # Create your views here.
 from products.models import Variation
@@ -119,3 +120,25 @@ class CartView(SingleObjectMixin, View):
         template = self.template_name
 
         return render(request, template, context)
+
+class CheckOutView(DetailView):
+    model = Cart
+    template_name = "carts/checkout_view.html"
+
+    def get_object(self, *args, **kwargs):
+        cart_id = self.request.session.get("cart_id")
+        if cart_id == None:
+            return redirect("cart")
+        cart = Cart.objects.get(id=cart_id)
+        return cart
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(CheckOutView, self).get_context_data(*args, **kwargs)
+        user_can_continue = False
+        if not self.request.user.is_authenticated():
+            context["login_form"] = AuthenticationForm()
+            context["next_url"] = self.request.build_absolute_uri()
+        if self.request.user.is_authenticated():
+            user_can_continue = True
+        context["user_can_continue"] = user_can_continue
+        return context
