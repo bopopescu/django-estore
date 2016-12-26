d802ad6d1a41ec81009aea59215d50623acfa6c5
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2016-12-26 17:36:48 +1100

    099 Crafting the Order Part 2

--- src/carts/views.py
+++ src/carts/views.py
@@ -170,6 +170,7 @@ class CheckOutView(DetailView, FormMixin):
             user_checkout.save()
             self.request.session["user_checkout_id"] = user_checkout.id
 
+        context["order"] = self.get_order()
         context["user_can_continue"] = user_can_continue
         context["form"] = self.get_form()
         return context
